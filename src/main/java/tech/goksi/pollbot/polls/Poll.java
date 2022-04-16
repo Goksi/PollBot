e1ee0c88eac14774c5ebe5ec99db6b877808a7a3
Goksi <admin@goksi.co.uk>
2022-04-16 22:37:53 +0200

    Added util class

--- src/main/java/tech/goksi/pollbot/polls/Poll.java
+++ src/main/java/tech/goksi/pollbot/polls/Poll.java
@@ -1,30 +1,76 @@
 package tech.goksi.pollbot.polls;
 
 
+
+import net.dv8tion.jda.api.entities.User;
 import tech.goksi.pollbot.Bot;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public abstract class Poll {
+public  class Poll {
+
+    enum Types{
+        YESNO,
+        SELECTMENU
+    }
     private String name;
-    private List<Integer> voted = new ArrayList<>();
-    private List<Integer> options = new ArrayList<>();
-    Poll(String name){
+    private List<Long> voted = new ArrayList<>();
+    private Map<String, List<Long>>  options = new HashMap<>(); //Option, list of users who voted for that option
+    private Types type;
+
+    Poll(String name, Types type){
         this.name = name;
+        this.type = type;
     }
 
-    abstract public void addOption(int option);
-    public void createPool(List<Integer> options){
-        try{
-            PreparedStatement ps = Bot.getInst().getSql().getConnection().prepareStatement("");
-        }catch (SQLException e){
-
+    public void addOption(){
+        if(type.equals(Types.YESNO)) {
+            options.put("YES", Collections.emptyList());
+            options.put("NO", Collections.emptyList());
         }
-
+    }
+    public void addOption(List<String> options){
+        for(String s : options){
+            this.options.put(s, Collections.emptyList());
+        }
     }
 
+    public List<String> getOptions(){
+        List<String> temp = new ArrayList<>();
+        for(Map.Entry<String, List<Long>> e : options.entrySet()) temp.add(e.getKey());
+        return temp;
+    }
 
+    public boolean hasVoted(User u){
+        for(Map.Entry<String, List<Long>> e : options.entrySet()){
+            if(e.getValue().contains(u.getIdLong())) return true;
+        }
+        return false;
+    }
+
+    public int getVoteCount(String option){
+        for(Map.Entry<String, List<Long>> e : options.entrySet()){
+            if(e.getKey().equals(option)){
+                return e.getValue().size();
+            }
+        }
+        return 0;
+    }
+
+    public void addVote(String option, User u){
+        for(Map.Entry<String, List<Long>> e : options.entrySet()){
+            if(e.getKey().equals(option))e.getValue().add(u.getIdLong());
+        }
+    }
+    public void save(){
+        Bot.getInst().getPolls().put(name, this);
+    }
+
+    public void end(){
+        Bot.getInst().getPolls().remove(name);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
